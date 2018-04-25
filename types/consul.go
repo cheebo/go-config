24aed46a16604314ffe5a91c77c2268cac0c9b3b
Sergey <serge@cheebo.me>
2018-04-25 05:20:33 +0500

    added tags for go-config

--- types/consul.go
+++ types/consul.go
@@ -1,15 +1,15 @@
 package types
 
 type ConsulTlsConfig struct {
-	CAFile   string `json:"cafile"`
-	CAPath   string `json:"capath"`
-	CertFile string `json:"certfile"`
-	KeyFile  string `json:"keyfile"`
+	CAFile   string `json:"cafile" cfg:"cafile"`
+	CAPath   string `json:"capath" cfg:"capath"`
+	CertFile string `json:"certfile" cfg:"certfile"`
+	KeyFile  string `json:"keyfile" cfg:"keyfile"`
 }
 
 type ConsulConfig struct {
 	Addr       string          `json:"addr" default:"localhost:8500"`
-	Datacenter string          `json:"dc" default:"dc1"`
+	Datacenter string          `json:"dc" cfg:"dc" default:"dc1"`
 	Token      string          `json:"token"`
 	Scheme     string          `json:"scheme" default:"http"`
 	Tls        ConsulTlsConfig `json:"tls"`
