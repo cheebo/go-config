013a17f0e3915fc8a895e1734a1209a8ff215bbc
Sergey <serge@cheebo.me>
2018-07-23 07:43:53 +0300

    flags source sceleton

--- sources/flag/flag.go
+++ sources/flag/flag.go
@@ -2,12 +2,10 @@ package flag
 
 import (
 	"flag"
-	"os"
-	"strings"
-
 	"github.com/cheebo/go-config"
-	"github.com/cheebo/go-config/utils"
+	"github.com/spf13/cast"
 	"reflect"
+	"strings"
 )
 
 type flags struct {
@@ -22,113 +20,75 @@ func Source() go_config.Source {
 	}
 }
 
-func (self *flags) Init(vals map[string]*go_config.Variable) error {
-	for name, val := range vals {
-		name = self.name(name)
-		switch val.Type.Kind() {
-		case reflect.String:
-			self.values[name] = self.fs.String(name, val.Def.Interface().(string), val.Description)
-
-		case reflect.Slice:
-			defVal := ""
-			self.values[name] = self.fs.String(name, defVal, val.Description)
-
-		case reflect.Int:
-			i, ok := val.Def.Interface().(int)
-			if !ok {
-				continue
-			}
-			self.values[name] = self.fs.Int(name, i, val.Description)
-
-		case reflect.Uint:
-			i, ok := val.Def.Interface().(uint)
-			if !ok {
-				continue
-			}
-			self.values[name] = self.fs.Uint(name, i, val.Description)
-
-		case reflect.Float64:
-			i, ok := val.Def.Interface().(float64)
-			if !ok {
-				continue
-			}
-			self.values[name] = self.fs.Float64(name, i, val.Description)
-
-		case reflect.Bool:
-			self.values[name] = self.fs.Bool(name, val.Def.Interface().(bool), val.Description)
-		}
-	}
-	var args []string
-	for _, f := range os.Args[:] {
-		if f[0] == '-' {
-			args = append(args, f)
-		}
-	}
-	return self.fs.Parse(args)
+func (f *flags) Get(key string) interface{} {
+	return f.lookup(f.key(key))
 }
 
-func (self *flags) Int(name string) (int, error) {
-	val, ok := self.values[self.name(name)]
-	if !ok {
-		return 0, nil
+func (f *flags) Bool(key string) (bool, error) {
+	val := f.lookup(f.key(key))
+	if val == nil {
+		return false, go_config.NoVariablesInitialised
 	}
-	i, ok := val.(*int)
-	return *i, nil
+	return cast.ToBoolE(val)
 }
 
-func (self *flags) Float(name string) (float64, error) {
-	val, ok := self.values[self.name(name)]
-	if !ok {
-		return 0, nil
+func (f *flags) Float(key string) (float64, error) {
+	val := f.lookup(f.key(key))
+	if val == nil {
+		return 0, go_config.NoVariablesInitialised
 	}
-
-	return float64(*val.(*float64)), nil
+	return cast.ToFloat64E(val)
 }
 
-func (self *flags) UInt(name string) (uint, error) {
-	val, ok := self.values[self.name(name)]
-	if !ok {
-		return 0, nil
+func (f *flags) Int(key string) (int, error) {
+	val := f.lookup(f.key(key))
+	if val == nil {
+		return 0, go_config.NoVariablesInitialised
 	}
-	return uint(*val.(*uint)), nil
+	return cast.ToIntE(val)
 }
 
-func (self *flags) String(name string) (string, error) {
-	val, ok := self.values[self.name(name)]
-	if !ok {
-		return "", nil
+func (f *flags) UInt(key string) (uint, error) {
+	val := f.lookup(f.key(key))
+	if val == nil {
+		return 0, go_config.NoVariablesInitialised
 	}
-	return *(val.(*string)), nil
+	return cast.ToUintE(val)
 }
 
-func (self *flags) Bool(name string) (bool, error) {
-	val, ok := self.values[self.name(name)]
-	if !ok {
-		return false, nil
-	}
-	b, ok := val.(*bool)
-	if !ok {
-		return false, nil
-	}
-	return *b, nil
+func (f *flags) Slice(key, delimiter string, kind reflect.Kind) ([]interface{}, error) {
+	return []interface{}{}, nil
 }
 
-func (self *flags) Slice(name, delimiter string, kind reflect.Kind) ([]interface{}, error) {
-	val, ok := self.values[self.name(name)]
-	if !ok {
-		return []interface{}{}, nil
+func (f *flags) String(key string) (string, error) {
+	val := f.lookup(f.key(key))
+	if val == nil {
+		return "", go_config.NoVariablesInitialised
 	}
-
-	src := *(val.(*string))
-
-	return utils.ParseSlice(src, delimiter, kind)
+	return cast.ToStringE(val)
 }
 
-func (self *flags) Export(opt ...go_config.SourceOpt) ([]byte, error) {
-
-	return []byte{}, nil
+func (f *flags) StringMap(key string) map[string]interface{} {
+	val := f.lookup(f.key(key))
+	if val == nil {
+		return map[string]interface{}{}
+	}
+	return cast.ToStringMap(val)
 }
 
-func (self *flags) name(name string) string {
-	return strings.ToLower(name)
+func (f *flags) IsSet(key string) bool {
+	val := f.lookup(f.key(key))
+	if val == nil {
+		return false
+	}
+	return true
+}
+
+func (f *flags) key(key string) string {
+	return strings.ToLower(key)
+}
+
+func (f *flags) lookup(flag string) interface{} {
+	// @todo implement method: get flag value
+	return nil
 }
