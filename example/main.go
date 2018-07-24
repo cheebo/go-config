a5f73ee6a9de07001b233a5993e8808520147cb0
Sergey <serge@cheebo.me>
2018-07-24 05:32:52 +0300

    added Unmarshal

--- example/main.go
+++ example/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"github.com/cheebo/go-config"
 	"github.com/cheebo/go-config/sources/env"
 	"github.com/cheebo/go-config/sources/file"
+	"github.com/cheebo/go-config/types"
+	"github.com/davecgh/go-spew/spew"
 )
 
 func main() {
@@ -15,8 +16,14 @@ func main() {
 	}
 	cfg.UseSource(env.Source("GO"), env.Source(""), fs)
 
-	fmt.Println(cfg.Get("name"), cfg.IsSet("name"))
-	fmt.Println(cfg.Get("age"), cfg.IsSet("age"))
-	fmt.Println(cfg.Get("amqp.url"), cfg.IsSet("amqp.url"))
-	fmt.Println(cfg.Get("amqp.addr"), cfg.IsSet("amqp.addr"))
+	//fmt.Println(cfg.Get("name"), cfg.IsSet("name"))
+	//fmt.Println(cfg.Get("age"), cfg.IsSet("age"))
+	//fmt.Println(cfg.Get("amqp.url"), cfg.IsSet("amqp.url"))
+	//fmt.Println(cfg.Get("amqp.addr"), cfg.IsSet("amqp.addr"))
+	m := types.AMQPConfig{}
+	err = cfg.Unmarshal(&m, "amqp")
+	if err != nil {
+		println(err.Error())
+	}
+	spew.Dump(m)
 }
--- go_config.go
+++ go_config.go
@@ -2,6 +2,9 @@ package go_config
 
 import (
 	"errors"
+	"github.com/spf13/cast"
+	"reflect"
+	"strings"
 )
 
 type config struct {
@@ -12,6 +15,7 @@ var (
 	GoConfig Config
 
 	NoVariablesInitialised = errors.New("no variables initialised")
+	NotAStructPtr          = errors.New("expects pointer to a struct")
 )
 
 func init() {
@@ -29,8 +33,16 @@ func (gc *config) UseSource(sources ...Source) {
 }
 
 func (gc *config) Unmarshal(v interface{}, prefix string) error {
-	// @todo implement
-	return NoVariablesInitialised
+	ptr := reflect.ValueOf(v)
+	if ptr.Kind() != reflect.Ptr {
+		return NotAStructPtr
+	}
+	ref := ptr.Elem()
+	if ref.Kind() != reflect.Struct {
+		return NotAStructPtr
+	}
+
+	return gc.setup(v, prefix)
 }
 
 func (gc *config) Get(key string) interface{} {
@@ -108,4 +120,66 @@ func (gc *config) IsSet(key string) bool {
 		}
 	}
 	return false
+}
+
+func (gc *config) setup(v interface{}, parent string) error {
+	refVal := reflect.ValueOf(v)
+
+	if refVal.Kind() == reflect.Ptr {
+		refVal = refVal.Elem()
+	}
+
+	if refVal.Kind() != reflect.Struct {
+		return nil
+	}
+
+	refType := reflect.TypeOf(refVal.Interface())
+
+	for i := 0; i < refVal.NumField(); i++ {
+		field := refType.Field(i)
+		refField := refVal.Field(i)
+
+		name := strings.ToLower(field.Name)
+		if len(parent) > 0 {
+			name = parent + "." + name
+		}
+
+		if refField.Kind() == reflect.Ptr {
+			if refField.IsNil() {
+				refField = reflect.New(refField.Type().Elem())
+				refVal.Field(i).Set(refField)
+				refField = refField.Elem()
+			} else {
+				refField = refField.Elem()
+			}
+		}
+
+		if refField.Kind() == reflect.Struct {
+			gc.setup(refField.Addr().Interface(), name)
+			continue
+		}
+
+		if !refField.CanSet() {
+			continue
+		}
+
+		switch refField.Kind() {
+		case reflect.Int:
+			refField.SetInt(cast.ToInt64(gc.Get(name)))
+		case reflect.Uint:
+			refField.SetUint(cast.ToUint64(gc.Get(name)))
+		case reflect.Float64:
+			refField.SetFloat(cast.ToFloat64(gc.Get(name)))
+		case reflect.Bool:
+			refField.SetBool(cast.ToBool(gc.Get(name)))
+		case reflect.String:
+			refField.SetString(cast.ToString(gc.Get(name)))
+		case reflect.Slice:
+			// @todo fill slice
+		case reflect.Map:
+			// @todo fill map
+		}
+
+	}
+	return nil
 }
