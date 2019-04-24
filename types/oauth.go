05e3bf0a58e61c4abd291abe0a25cf918244307a
Sergey <serge@cheebo.me>
2019-04-24 09:57:34 +0500

    removed obsolet structures

--- types/oauth.go
+++
@@ -1,8 +0,0 @@
-package types
-
-type OAuthProvider struct {
-	Provider string `json:"provider"`
-	Key      string `json:"key"`
-	Secret   string `json:"secret"`
-	Callback string `json:"callback"`
-}
